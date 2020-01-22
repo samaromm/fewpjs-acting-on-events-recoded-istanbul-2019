d6141d74879ca9c2fc4e938f1ded437a5082d965
Samar Omar <samaromm98@gmail.com>
2020-01-22 22:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(rightNumbers, 10);
  
-  if (right > 0) {
+  if (left > 0) {
     dodger.style.left = `${left + 1}px`;
   }
 }
